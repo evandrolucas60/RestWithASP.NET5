5050f0305aea8ab2ad4f4076d96dee7f6253cba9
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-14 10:24:38 -0300

    Creating a Context

---
+++ RestWithASPNET5_Person/RestWithASPNET5/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNET5.Model;
+
+namespace RestWithASPNET5.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> persons { get; set; }
+    }
+}
