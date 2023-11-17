1ddfea936040529910f386148503da7a4679e21b
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-17 16:01:19 -0300

    Populating database

--- RestWithASPNET5_Person/RestWithASPNET5/Context/ApplicationDbContext.cs
+++ RestWithASPNET5_Person/RestWithASPNET5/Context/ApplicationDbContext.cs
@@ -12,5 +12,20 @@ namespace RestWithASPNET5.Context
         }
 
         public DbSet<Person> persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ensure the base OnModelCreating is called first
+            base.OnModelCreating(modelBuilder);
+
+            // Add initial data to your table
+            modelBuilder.Entity<Person>().HasData(
+                    new Person { Id = 1, Address = "Recife", FirstName = "Evandro", Gender = "Male", LastName = "Lucas"},
+                    new Person { Id = 2, Address = "Jaboatão", FirstName = "Jamerson", Gender = "Male", LastName = "Lucas"},
+                    new Person { Id = 3, Address = "Petrolina", FirstName = "Danielly", Gender = "Femele", LastName = "Oliveira"},
+                    new Person { Id = 4, Address = "Juazeiro", FirstName = "Lucinea", Gender = "Femele", LastName = "Lucas"},
+                    new Person { Id = 5, Address = "Recife", FirstName = "Bruno", Gender = "male", LastName = "Lucas"}
+                );
+        }
     }
 }
