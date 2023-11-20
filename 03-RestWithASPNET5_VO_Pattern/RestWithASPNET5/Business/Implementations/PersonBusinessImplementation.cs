230eb1782a30e1511ad5ff9ed997cb8aa897ae0a
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-20 16:55:27 -0300

    The VO(Value Object) Pattern

---
+++ 03-RestWithASPNET5_VO_Pattern/RestWithASPNET5/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASPNET5.Data.Converter.Implementations;
+using RestWithASPNET5.Data.VO;
+using RestWithASPNET5.Model;
+using RestWithASPNET5.Repository;
+
+namespace RestWithASPNET5.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+        private readonly PersonConverter _coverter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _coverter = new PersonConverter();
+        }
+
+        //Method responsible for creating a person
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _coverter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _coverter.Parse(personEntity);
+        }
+
+        //Method responsible for deleting a PersonVO from an Id
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        //Method responsible for return a list of PersonVO
+        public List<PersonVO> FindAll()
+        {
+            return _coverter.Parse(_repository.FindAll());
+        }
+
+
+        //Method responsible for return a PersonVO from a ID
+        public PersonVO FindByID(long id)
+        {
+            return _coverter.Parse(_repository.FindByID(id));
+        }
+
+
+        //Method responsible for Update a PersonVO information
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _coverter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _coverter.Parse(personEntity);
+        }
+    }
+}
