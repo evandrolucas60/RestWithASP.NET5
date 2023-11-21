f764b2fe3da927ee2d3cc37207c882b93929f893
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-21 08:34:05 -0300

    Setting the media type format to accept XML format in class program

---
+++ 04-RestWithASPNET5_Content_Negociation/RestWithASPNET5/Business/IPersonBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET5.Data.VO;
+
+namespace RestWithASPNET5.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindByID(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        void Delete(long id);
+    }
+}
