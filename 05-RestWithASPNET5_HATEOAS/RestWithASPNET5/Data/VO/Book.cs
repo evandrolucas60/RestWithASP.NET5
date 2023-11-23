447fc279d2d19d7cd52e7294d75b56c42cc10195
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-23 15:06:37 -0300

    implementing the ISupportsHyperMedia Interface in BookVO

--- 05-RestWithASPNET5_HATEOAS/RestWithASPNET5/Data/VO/Book.cs
+++ 05-RestWithASPNET5_HATEOAS/RestWithASPNET5/Data/VO/Book.cs
@@ -1,11 +1,15 @@
-﻿namespace RestWithASPNET5.Model
+﻿using RestWithASPNET5.Hypermedia;
+using RestWithASPNET5.Hypermedia.Abstract;
+
+namespace RestWithASPNET5.Model
 {
-    public class BookVO
+    public class BookVO : ISupportsHyperMedia
     {
         public long Id { get; set; }
         public string Author { get; set; }
         public DateTime Date { get; set; }
         public double Price { get; set; }
         public string Title { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
     }
 }
