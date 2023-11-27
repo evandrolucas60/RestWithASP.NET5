d7510bb77e859a12f0c5e8c0a8c56437140083ea
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-27 13:58:57 -0300

    Creating the Class UserRepository that implements the IUserRepository Interface

---
+++ 05-RestWithASPNET5_HATEOAS/RestWithASPNET5/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using RestWithASPNET5.Context;
+using RestWithASPNET5.Data.VO;
+using RestWithASPNET5.Model;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASPNET5.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+                
+            return _context.users.FirstOrDefault(u => (u.Username == user.Username) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, SHA256 algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
