dfabbfb5c5e91e419395e89c1ea1ea75df3437f5
evandrolucas60 <evandrolucas60@yahoo.com>
2023-11-27 15:09:51 -0300

    Creating the folder services that contains the ITokenInterface

---
+++ 05-RestWithASPNET5_HATEOAS/RestWithASPNET5/Services/ITokenInterface.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace RestWithASPNET5.Services
+{
+    public interface ITokenInterface
+    {
+        string GenerateAcessToken(IEnumerable<Claim> clains);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetClaimsPrincipalFromExpiredToken(string token);
+    }
+}
