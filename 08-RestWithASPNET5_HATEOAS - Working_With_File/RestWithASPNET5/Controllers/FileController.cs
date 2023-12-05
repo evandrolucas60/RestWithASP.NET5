ab0e842155e15545125622e0232bc7134ac51aff
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-05 17:06:15 -0300

    creating a controller to named FileController

---
+++ 08-RestWithASPNET5_HATEOAS - Working_With_File/RestWithASPNET5/Controllers/FileController.cs
@@ -0,0 +1,44 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET5.Business;
+using RestWithASPNET5.Data.VO;
+
+namespace RestWithASPNET5.Controllers
+{
+
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class FileController : Controller
+    {
+        private readonly IFileBusiness _fileBusiness;
+
+        public FileController(IFileBusiness fileBusiness)
+        {
+            _fileBusiness = fileBusiness;
+        }
+
+        [HttpPost("uploadFile")]
+        [ProducesResponseType((200), Type = typeof(FileDetailVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/json")]
+        public async Task<IActionResult> UploadOneFile([FromForm] IFormFile file)
+        {
+            FileDetailVO detail = await _fileBusiness.SaveFileToDisk(file);
+            return new OkObjectResult(detail);
+        }
+        
+        
+        [HttpPost("uploadMultipleFiles")]
+        [ProducesResponseType((200), Type = typeof(List<FileDetailVO>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/json")]
+        public async Task<IActionResult> UploadManyFiles([FromForm] List<IFormFile> files)
+        {
+            List<FileDetailVO> details = await _fileBusiness.SaveFilesToDisk(files);
+            return new OkObjectResult(details);
+        }
+    }
+}
