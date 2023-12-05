067e1fa739a065613f05eee83148e7f0e061cfab
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-05 14:32:07 -0300

    Implementig the IFileBusiness Interface in the class FileBusinessImplementation

---
+++ 08-RestWithASPNET5_HATEOAS - Working_With_File/RestWithASPNET5/Business/Implementations/FileBusinessImplementation.cs
@@ -0,0 +1,31 @@
+﻿using RestWithASPNET5.Data.VO;
+
+namespace RestWithASPNET5.Business.Implementations
+{
+    public class FileBusinessImplementation : IFileBusiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusinessImplementation(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
