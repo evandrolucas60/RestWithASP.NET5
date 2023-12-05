ec7d5a8c5ba3194a9007b06c8bb1577162bef5b5
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-05 15:10:36 -0300

    implementing the SaveFileToDisk method resposible to save the files

--- 08-RestWithASPNET5_HATEOAS - Working_With_File/RestWithASPNET5/Business/Implementations/FileBusinessImplementation.cs
+++ 08-RestWithASPNET5_HATEOAS - Working_With_File/RestWithASPNET5/Business/Implementations/FileBusinessImplementation.cs
@@ -17,15 +17,36 @@ namespace RestWithASPNET5.Business.Implementations
         {
             throw new NotImplementedException();
         }
+        public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            FileDetailVO fileDetail = new FileDetailVO();
+
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || 
+                fileType.ToLower() == ".png" || fileType.ToLower() == "jpeg")
+            {
+                var docName = Path.GetFileName(file.FileName);
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(_basePath, "", docName);
+                    fileDetail.DocumentName = docName;
+                    fileDetail.DocType = fileType;
+                    fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1" + fileDetail.DocumentName);
+
+                    //p.s. Search how to implementing that stream in a database
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return fileDetail;
+        }
 
         public Task<FileDetailVO> SaveFilesToDisk(IList<IFormFile> file)
         {
             throw new NotImplementedException();
         }
-
-        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
-        {
-            throw new NotImplementedException();
-        }
     }
+
 }
