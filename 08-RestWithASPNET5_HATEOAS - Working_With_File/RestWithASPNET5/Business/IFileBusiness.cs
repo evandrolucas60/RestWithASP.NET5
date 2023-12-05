fa31fe507f44612e17513136898d0fc213ed6f17
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-05 14:30:22 -0300

    Implementing the IFileBusiness Interface

---
+++ 08-RestWithASPNET5_HATEOAS - Working_With_File/RestWithASPNET5/Business/IFileBusiness.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASPNET5.Data.VO;
+
+namespace RestWithASPNET5.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<FileDetailVO> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
