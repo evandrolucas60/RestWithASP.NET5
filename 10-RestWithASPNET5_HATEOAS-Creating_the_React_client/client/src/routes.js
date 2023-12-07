fe83a397abfcbb64b4aadf46b4f774763be08fbe
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-07 14:40:03 -0300

    starting the book screen implementation

--- 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/routes.js
+++ 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Login from "./pages/Login";
+import Book from "./pages/Book";
 
 export default function MyRoutes() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" Component={Login}/>
+                <Route path="/" exact Component={Login}/>
+                <Route path="/book" Component={Book}/>
             </Routes>
         </BrowserRouter>
     );
