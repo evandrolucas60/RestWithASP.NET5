cf154177a3b2b18a3d4b4e7706fc8bed103aff93
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-07 16:04:04 -0300

    Styling the Books Screen Listing

--- 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/pages/Book/index.js
+++ 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/pages/Book/index.js
@@ -181,6 +181,24 @@ export default function Book() {
                         <FiTrash2 size={20} color="#251fc5" />
                     </button>
                 </li>
+                <li>
+                    <strong>Title</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251fc5" />
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251fc5" />
+                    </button>
+                </li>
             </ul>
         </div>
     );
