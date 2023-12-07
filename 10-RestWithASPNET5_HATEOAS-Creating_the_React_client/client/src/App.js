3d4638c91b3506ad038c9eab6d9e30f2bc6fd4f5
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-07 11:20:29 -0300

    Implementing the login structure

--- 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/App.js
+++ 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/App.js
@@ -1,21 +1,11 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import React from 'react';
+import Login from './pages/Login';
+import './global.css';
 
  
  export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter (counter + 1);
-  }
-
   return(
-    <div>
-    <Header>
-      Counter: {counter}
-    </Header>
-    <button onClick={increment}>Add</button>
-    </div>
+    <Login/>
   );
 }
 
