e4bab41c8adc1ae5e023edfbae3f8398f21c602f
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-07 10:59:11 -0300

    State concept

--- 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/App.js
+++ 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/client/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './Header';
 
  
  export default function App() {
-  return(<h1>Hello World!</h1>);
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter (counter + 1);
+  }
+
+  return(
+    <div>
+    <Header>
+      Counter: {counter}
+    </Header>
+    <button onClick={increment}>Add</button>
+    </div>
+  );
 }
 
