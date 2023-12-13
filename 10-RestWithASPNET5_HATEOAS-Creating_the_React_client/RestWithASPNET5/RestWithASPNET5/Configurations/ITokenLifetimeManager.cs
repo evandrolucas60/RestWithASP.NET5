936840bb94a686c49d09914bb8a931be871e21be
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-13 10:49:16 -0300

    creating a interface to control the token lifetime

---
+++ 10-RestWithASPNET5_HATEOAS-Creating_the_React_client/RestWithASPNET5/RestWithASPNET5/Configurations/ITokenLifetimeManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace RestWithASPNET5.Configurations
+{
+    public interface ITokenLifetimeManager
+    {
+        bool ValidateToken(DateTime? notBefore,
+                      DateTime? expires,
+                      SecurityToken securityToken,
+                      TokenValidationParameters validationParameters);
+
+        void SignOut(SecurityToken securityToken);
+    }
+}
